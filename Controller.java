96635b6917bae037727f579caeb32b485a072762
Azra Animating <42267730+AzraAnimating@users.noreply.github.com>
2020-01-16 20:20:45 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -30,10 +30,81 @@ public class Controller
         }
         System.out.println(lGesamt);
     }
-    public void contains57()
-    {
-        aZahlenliste.toFirst();
-        int lAmoutOf57s = 0;
-        while(aZ
+
+    public void contains(int lNumber){
+        int lAmountOfNumbers = 0;
+        int i = 0;
+        aZahlenListe.toFirst();
+        while (aZahlenListe.hasAccess())
+        {
+            if(aZahlenListe.getContent() == lNumber)
+            {
+                lAmountOfNumbers++;
+            }
+            aZahlenListe.next();
+        }
+        System.out.println(lAmountOfNumbers);
+
     }
+
+    public void containsAt(int lNumber)
+    {
+        int i = 0;
+        aZahlenListe.toFirst();
+        while (aZahlenListe.hasAccess())
+        {
+            if(aZahlenListe.getContent() == lNumber)
+            {
+                System.out.println(lNumber + " an Index " + i);
+            }
+            aZahlenListe.next();
+            i++;
+        }
+    }
+
+    public void removeThingsThatContain30()
+    {
+        aZahlenListe.toFirst();
+        while (aZahlenListe.hasAccess())
+        {
+            if(aZahlenListe.getContent().toString().contains("30")){
+                aZahlenListe.remove();
+                System.out.println("removed");
+            }
+            aZahlenListe.next();
+        }
+    }
+
+    public void add1k(int lIndex)
+    {
+        aZahlenListe.toFirst();
+        for (int i = 0 ; i < lIndex - 1; i++){
+            aZahlenListe.next();
+        }
+
+        aZahlenListe.insert(10000);
+    }
+
+    public void printFibonacciZahlen(){
+        long lLetzteZahl = 0;
+        long lZahlDavor = 1;
+        long lFibonacciZahl = 0;
+
+        List<Long> lFibonacciListe = new List<Long>();
+
+        lFibonacciListe.toFirst();
+
+        while (lFibonacciZahl < 1000000000)
+        {
+            lFibonacciZahl = lZahlDavor + lLetzteZahl;
+            lZahlDavor = lFibonacciZahl;
+            lFibonacciListe.append(lFibonacciZahl);
+            System.out.println(lFibonacciZahl);
+            lFibonacciZahl = lLetzteZahl + lZahlDavor;
+            lLetzteZahl = lFibonacciZahl;
+            System.out.println(lFibonacciZahl);
+        }
+    }
+
+
 }
